5742e167eb6acac3c512b1578ffb08cc1bae23df
Mohammad Hossein Heydarchi <mhheidarchi2003@yahoo.com>
2025-04-03 16:43:15 +0200

    Update app (#1)
    * - Upgrade modules and packages
    - Example of using a protobuf message
    - Add some code to store and load data from file
    * Add GitHub action to build
    * Print as byre array to compare in logcat

--- app/src/main/java/com/example/protoexample/FirstFragment.kt
+++ app/src/main/java/com/example/protoexample/FirstFragment.kt
@@ -1,18 +1,23 @@
 package com.example.protoexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.proto.CustomMessage
+import com.example.proto.CustomMessage.OtherReceiver
 import com.example.protoexample.databinding.FragmentFirstBinding
+import java.io.File
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private val TAG = "ProtoBufApp"
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -32,9 +37,53 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.storeCustomMessage.setOnClickListener {
+            saveCustomMessage(
+                CustomMessage.newBuilder()
+                    .setTitle("Hello")
+                    .setId(1)
+                    .setPriority(CustomMessage.Priority.URGENT)
+                    .setDateTime(System.currentTimeMillis().toString())
+                    .setSenderName("John Doe")
+                    .addReceivers(OtherReceiver.newBuilder().setReceiverName("Jane Wiliams").build())
+                    .build()
+            )
         }
+
+        binding.loadCustomMessage.setOnClickListener {
+            val customMessage = loadCustomMessage()
+
+            val byteArray = customMessage.toByteArray()
+            val hexString = byteArray.joinToString(" ") { "%02X".format(it) }
+            Log.d("TAG", "CustomMessage as ByteArray (hex): $hexString")
+
+            Log.v(TAG, customMessage.toString())
+        }
+
+    }
+
+    private fun saveCustomMessage(customMessage: CustomMessage){
+        val byteArray = customMessage.toByteArray()
+        val hexString = byteArray.joinToString(" ") { "%02X".format(it) }
+        Log.d("TAG", "CustomMessage as ByteArray (hex): $hexString")
+
+        Log.v(TAG,"Custom message saved: $customMessage")
+        val file = File(context?.filesDir, "custom_message.tmp")
+        file.writeText(customMessage.toByteArray().toString(Charsets.UTF_8))
+
+    }
+
+    private fun loadCustomMessage(): CustomMessage {
+        // Load the custom message from a temp file
+
+        val file = File(context?.filesDir, "custom_message.tmp")
+        if (!file.exists()) {
+            Log.v(TAG,"No custom message found")
+            return CustomMessage.getDefaultInstance()
+        }
+        val bytes = file.readText(Charsets.UTF_8).toByteArray()
+        val customMessage = CustomMessage.parseFrom(bytes)
+        return customMessage
     }
 
     override fun onDestroyView() {
